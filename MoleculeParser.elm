fc18f31747b19cf614b2ed16f8dbcc750bf82730
Unknown <mattjdupree@gmail.com>
2016-10-24 13:11:05 -0700

    Rebuilt molecule parser for cleaner recursion. Gave up on parenthesis for now.
    It's still a mess, but it's on its way.

--- MoleculeParser.elm
+++ MoleculeParser.elm
@@ -1,58 +1,71 @@
-module MoleculeParser exposing (parseMolecule)
+module MoleculeParser exposing (..)
 
-import Result
+
 import String
 import Char
+import Result
+import Regex
 
-type alias Atom = 
-    { symbol : (Result String String)
-    , count : Int
-    , characters : Int
-    }
+
+moleculeRegexString : String
+moleculeRegexString = "[A-Z][a-z]?\\d*|\\([^()]*(?:\\(.*\\))?[^()]*\\)\\d+"
+moleculeRegex : Regex.Regex
+moleculeRegex = Regex.regex moleculeRegexString
+
 
 parseMolecule : String -> Result String (List String)
-parseMolecule input =
-    if String.isEmpty input then
-        Result.Ok []
-    else if (String.left 1 input) == "(" then
-        Result.Err "Unimplemented \"(\""
-    else if (String.left 1 input) == ")" then
-        Result.Err "Unimplemented \")\""
-    else if String.all Char.isUpper (String.left 1 input) then
-        let
-            atom = subParseAtom input
-        in
-            case atom.symbol of
-                Result.Ok symbol ->
-                    let
-                        myResult = (List.repeat atom.count symbol)
-                        theirResult = (parseMolecule (String.dropLeft atom.characters input))
-                    in
-                        case theirResult of
-                            Result.Ok theirList ->
-                                Result.Ok (List.append myResult theirList)
-                            Result.Err error ->
-                                theirResult
-                Result.Err error ->
-                    Result.Err error
-    else
-        Result.Err ("Unparseable symbol encountered: "++(String.left 1 input))
+parseMolecule input = 
+    let
+        elementList = List.map .match (Regex.find (Regex.All) moleculeRegex input)
+        resultList = List.map subParseMolecule elementList
+        failureList = List.filter isError resultList
+        successList = List.map (Result.withDefault [""]) resultList
+    in
+        if not (List.isEmpty failureList) then
+            Result.Err "Unspecified error occurred somewhere down the line -- Finish implementing error handling!"
+        else
+            Result.Ok (List.concat successList)
 
-subParseAtom : String -> Atom
-subParseAtom input = 
-    if String.isEmpty input then
-        Atom (Result.Err "No atom present.") 0 0
-    else if not (String.all Char.isUpper (String.left 1 input)) then
-        Atom (Result.Err "Imparseable character.") 0 0
-    else
-        let
-            symbolLength = 
-                if String.all Char.isLower (String.right 1 (String.left 2 input)) then
-                    2
-                else
-                    1 
-            symbol = String.right symbolLength input
-        in
-            Atom (Result.Ok symbol) 0 symbolLength
 
-                
+subParseMolecule : String -> Result String (List String)
+subParseMolecule element =
+    {-if (String.left 1 element) == "(" then
+        let
+            sansLeftParan = String.dropLeft 1 element
+            splitList = String.split ")" sansLeftParan
+            subMoleculeString = Result.fromMaybe "Bad submolecule string" (List.head splitList)
+            subMolecule = Result.map parseMolecule subMoleculeString
+            count = String.toInt (Maybe.withDefault "" (List.head (Maybe.withDefault [] (List.tail splitList))))
+        in
+            Result.map List.concat (Result.map2 List.repeat count subMolecule)
+    else-}
+        subParseElement element
+
+subParseElement : String  -> Result String (List String)
+subParseElement element =
+    if not (String.all Char.isUpper (String.left 1 element)) then
+        Result.Err ("No atomic symbol found at input: "++element)
+    else if String.any Char.isLower (String.left 2 element) then
+        let
+            count = Result.withDefault 1 (String.toInt (String.dropLeft 2 element))
+            symbol = String.left 2 element
+        in
+            Result.Ok (List.repeat count symbol)
+    else
+        let 
+            count = Result.withDefault 1 (String.toInt (String.dropLeft 1 element))
+            symbol = String.left 1 element
+        in
+            Result.Ok (List.repeat count symbol)
+
+isError : Result a b -> Bool
+isError input =
+    case input of
+        Result.Ok data -> False
+        Result.Err error -> True
+
+isOk : Result a b -> Bool
+isOk input =
+    case input of
+        Result.Ok data -> True
+        Result.Err error -> False
